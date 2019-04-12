95608f40f671b4ff3f2b1506092a2838a8cdde6f
Usman Haroon <haroonasr9@gmail.com>
2019-04-12 12:09:34 +0530

    Included wiki library
    Now this will answer all of your queries

--- Jarvis.py
+++ Jarvis.py
@@ -16,9 +16,9 @@ def greetMe():
         if (0 <= currenttime and currenttime <= 12):
             talk("Good Morning")
         elif(12 < currenttime and currenttime <= 17):
-            talk("Good Evening")
+            talk("Good Afternoon")
         else:
-            talk("Good Night")
+            talk("Good Evening")
 
 
 def talk(audio):
@@ -36,7 +36,8 @@ def myCommand():
         print('I said:' + query)
     except sr.UnknownValueError:
         talk("Sorry sir!,I don't understand.Please type your query")
-        query = str(input('Type your question:'))
+        strquery = input('Type your question:')
+        query = str(strquery)
 
     return query
 
@@ -46,22 +47,52 @@ voices = engine.getProperty('voices')       #getting details of current voice
 engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
 
 greetMe()
+today = datetime.datetime.now()
+text = "Today is" + str(today.strftime("%A"))
+
+talk(text)
 talk("I am Friday your virtual assistant")
 
-client = wolframalpha.Client("Your Client-ID")
+client = wolframalpha.Client("App_ID")
 
-query = myCommand()
-query = query.lower()
+while True:
+    query = myCommand()
 
-if 'open google' in query:
-    talk('As you wish')
-    webbrowser.open("www.google.co.in")
-elif 'open youtube' in query:
-    talk('Enjoy youtube')
-    webbrowser.open("www.youtube.com")
-elif 'open my github' in query:
-    talk('Opening your profile')
-    webbrowser.open("https://github.com/UsmanHaroon1")
-elif 'bye friday' or 'by friday' in query:
-    talk("See you soon")
-    sys.exit()
+    query = query.lower()
+
+    if 'open google' in query:
+        talk('As you wish')
+        webbrowser.open("www.google.co.in")
+    elif 'open youtube' in query:
+        talk('Enjoy youtube')
+        webbrowser.open("www.youtube.com")
+    elif 'open my github' in query:
+        talk('Opening your profile')
+        webbrowser.open("https://github.com/UsmanHaroon1")
+    elif "what\'s up" in query or 'how are you' in query:
+        Msgs = ['I am always fine', 'ya good!', 'Awesome', 'I am nice and full of energy']
+        talk(random.choice(Msgs))
+    elif 'bye friday' in query or 'by friday' in query:
+        talk("Bye man")
+        sys.exit()
+    elif 'stop' in query or 'abort' in query:
+        talk('Bye Sir, have a good day.')
+        sys.exit()
+    else:
+        query = query
+        print("Searching Query:" + query)
+        talk("Searching")
+        try:
+            try:
+                res = client.query(query)
+                result = next(res.results).text
+                talk("Jarvis says")
+                talk(result)
+            except:
+                result = wikipedia.summary(query,sentences=2)
+                talk("Wiki says")
+                talk(result)
+        except:
+            webbrowser.open("www.google.co.in")
+
+    talk("Next command")
